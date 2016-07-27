84ee1679d430e8a91dc9bbd5d936b48e23085d72
Edgar Quiquia <edgar.quiquia@tektonlabs.com>
2016-07-27 12:09:21 -0500

    They have improved styles

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-  var Trianglify = require('trianglify');
   var aosaa = require("aos-any-animation");
+  aosaa.init({});
+
+
+  
+(function mountClaimForm() {
+  
   var animation = function Animation() {
     var elm = this;
     var newone = elm.cloneNode(true);
     newone.addEventListener('click', animation);
     elm.parentNode.replaceChild(newone, elm);
   };
-(function mountClaimForm() {
-  
-  
   var cubes = document.getElementsByClassName("item__inner");
   for (i=0;i<cubes.length;i++){
     cubes[i].parentElement.addEventListener('click', animation);
-    cubes[i].style.background = 'url('+Trianglify({width: 300, height: 300}).png()+')';
   }
 
-  aosaa.init({});
 
 
 }());
